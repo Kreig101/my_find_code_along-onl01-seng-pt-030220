e22c3ff12f873b0d3b4da82b495d6fd469ede678
Craig Storer <craigstorer@gmail.com>
2020-03-11 17:22:42 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,7 +1,9 @@
     def my_find(collection)
       i = 0
-      while i < 100
-        yield(1)
-          i = i + 1
+      while i < collection.length
+        if yield(collection[i])
+          return collection[i]
+        end
+        i = i + 1
       end
     end
